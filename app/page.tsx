8a525483187f4867d7f492f51f6bd187824ca308
Parvez Kose <parvez.kosex@gmail.com>
2025-02-04 16:12:24 -0500

    modify bio

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,11 @@ export default function Page() {
         Parvez Kose
       </h1>
       <p className="mb-4">
-        {`Hello, I’m a Staff Software Engineer focussed on building intuitive and responsive digital experiences.`}
+        {`Hello, I’m a Staff Software Engineer focussed on building intuitive and responsive product experiences.`}
+        {/* {`Hello, I’m a Staff Software Engineer focussed on building intuitive and responsive digital experiences.`} */}
       </p>
       <p className="mb-4">
-        {`I love turning ideas into tangible products, prototyping user interfaces and web animations that push the envelope of what's possible and adds a level of craftsmanship to the site. I’m passionate about interaction design, visualization and building immersive product experiences. `}
+        {`I love turning ideas into tangible products, prototyping user interfaces and web animations that push the envelope of what's possible and adds a level of craftsmanship to the UI. I’m passionate about interaction design, visualization and building immersive product experiences. `}
       </p>
       <div className="my-8">{/* <BlogPosts /> */}</div>
     </section>
