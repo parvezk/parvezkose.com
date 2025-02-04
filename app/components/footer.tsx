04c873b45090a6275f2a1fed1c0a249d5fa850f6
Parvez Kose <parvez.kosex@gmail.com>
2025-02-04 10:10:16 -0500

    update footer link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://frontendeng.substack.com/"
+            href="https://frontendapps.substack.com/"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">substack</p>
