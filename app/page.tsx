3347f5f4183ae8159c2166c1c57bd9ef5e8f4ddb
Parvez Kose <parvez.kosex@gmail.com>
2025-02-19 11:05:37 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,25 @@ export default function Page() {
       </h1>
       <p className="mb-4">
         {`Hello, I’m a Staff Software Engineer focussed on building intuitive and responsive product experiences.`}
-        {/* {`Hello, I’m a Staff Software Engineer focussed on building intuitive and responsive digital experiences.`} */}
       </p>
       <p className="mb-4">
-        {`I love turning ideas into tangible products, prototyping user interfaces and web animations that push the envelope of what's possible and adds a level of craftsmanship to the UI. I’m passionate about interaction design, visualization and building immersive product experiences. `}
+        {`I turn ideas into products. Working on building responsive, high performing digital interfaces with exceptional user experience. 
+        I love prototyping interfaces and animations that push the envelope of what's possible and adds a level of craftsmanship to the product. I'm passionate about data visualization, and building immersive and dynamic user experience on web`}
       </p>
       <div className="my-8">{/* <BlogPosts /> */}</div>
     </section>
   );
 }
+
+/**
+ * 
+ * Hello, I’m a Staff Software Engineer focussed on building intuitive and responsive digital experiences.
+ * I love turning ideas into tangible products, prototyping user interfaces and web animations that push the envelope of what's possible and adds a level of craftsmanship to the UI. I’m passionate about interaction design, visualization and building immersive product experiences.
+ * 
+ * I turn ideas into products. Working on building responsive, high performing applications with exceptional user experience. 
+
+I turn ideas into products. I work on building digital interfaces that deliver exceptional user experiences for theend users. I love prototyping user interfaces and creating animations that push the envelope of what’s possible and adds a level of craftsmanship. I’m passionate data visualization and building dynamic user experience on web.
+
+build dynamic user experiences on the web
+pushing the boundaries of web performance and user experience. 
+ */
