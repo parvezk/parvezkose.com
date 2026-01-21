7b4f8a824f1598096c64f301658a1fb7563398d4
Parvez Kose <parvez.kosex@gmail.com>
2026-01-20 21:52:07 -0500

    update bio

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,13 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Parvez Kose
       </h1>
+      <h4>{`Staff Software Engineer | Full Stack and Generative AI`}</h4>
+      <br />
       <p className="mb-4">
-        {`Hello, I’m a Staff Software Engineer focussed on building intuitive and responsive product experiences.`}
+        {`I design and build AI-powered systems that automate complex workflows and optimize cost at scale.`}
       </p>
       <p className="mb-4">
-        {`I turn ideas into products. Working on building responsive, high performing digital interfaces with exceptional user experience. 
-        I love prototyping interfaces and animations that push the envelope of what's possible and adds a level of craftsmanship to the product. I'm passionate about data visualization and building dynamic user experience on web`}
+        {`I love creating product experience that push the envelope of what’s possible with user experience and add a level of craftsmanship to the work. I'm passionate about data visualization and building immersive experience for the users.`}
       </p>
       <div className="my-8">{/* <BlogPosts /> */}</div>
     </section>
@@ -19,11 +20,16 @@ export default function Page() {
 }
 
 /**
+ *
+ * Hello, I’m a Staff Software Engineer focussed on building intuitive and responsive product experiences.
+ * I turn ideas into products. Working on building responsive, high performing digital interfaces with exceptional user experience.
+        I love prototyping interfaces and animations that push the envelope of what's possible and adds a level of craftsmanship to the product. I'm passionate about data visualization and building dynamic user experience on web
+
  * immersive interaction designs
  * Hello, I’m a Staff Software Engineer focussed on building intuitive and responsive digital experiences.
  * I love turning ideas into tangible products, prototyping user interfaces and web animations that push the envelope of what's possible and adds a level of craftsmanship to the UI. I’m passionate about interaction design, visualization and building immersive product experiences.
- * 
- * I turn ideas into products. Working on building responsive, high performing applications with exceptional user experience. 
+ *
+ * I turn ideas into products. Working on building responsive, high performing applications with exceptional user experience.
 
 I turn ideas into products. I work on building digital interfaces that deliver exceptional user experiences for theend users. I love prototyping user interfaces and creating animations that push the envelope of what’s possible and adds a level of craftsmanship. I’m passionate data visualization and building dynamic user experience on web.
 
